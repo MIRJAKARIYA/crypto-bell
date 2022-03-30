1b6eea69943199c53e42c3570fa0d9cf6ac4a3e9
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-03-30 06:23:26 +0600

    sigle coin info added

--- src/components/CoinInfo/CoinInfo.js
+++ src/components/CoinInfo/CoinInfo.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const CoinInfo = () => {
+    const {coinId} = useParams();
+    const [coin, setCoin] = useState({});
+    useEffect( ()=>{
+        fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`)
+        .then(res => res.json())
+        .then(data => setCoin(data))
+    },[coinId]);
+    console.log(coin)
     return (
-        <div>
-            <h1>Coin information is here</h1>
+        <div className='grid md:grid-cols-2'>
+            <div className='border'>
+                <img src={coin.image?.large} className='w-9/12' alt="" />
+            </div>
+            <div className='border'>
+                
+            </div>
         </div>
     );
 };
